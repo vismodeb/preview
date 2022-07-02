d1d9c4c8c8c1a174da5c6bde9dcda19ce0e3f888
vismo_dev <105719010+vismodeb@users.noreply.github.com>
2022-07-02 19:00:57 +0600

    Add files via upload

---
+++ assets/js/min.js
@@ -0,0 +1,57 @@
+
+// Range
+function showVal1(newVal){
+  document.getElementById("valBox1").innerHTML = newVal;
+}
+function showVal2(newVal2){
+  document.getElementById("valBox2").innerHTML = newVal2;
+}
+
+
+function showVal3(newVal3){
+  document.getElementById("valBox3").innerHTML = newVal3;
+}
+
+
+// Coundown item
+
+// let i = 1;
+// function startCount(){
+//   const coundownStart = setInterval(myTimer,1000);
+//   function myTimer() {
+//     i++;
+//     document.querySelector('#ranCount').innerText = i;
+//     if(i < 10){
+
+//       document.querySelector('#ranCount').innerText = "0" + i;
+//     }else{
+//       document.querySelector('#ranCount').innerText = i;
+//     }
+//   }
+ 
+  
+// }
+// function stopCount(){
+//   clearInterval(coundownStart);
+// }
+
+
+
+
+// function startCount() {
+//   let i = 0;
+//  const stopintarval = setInterval(function(){
+//   i++;
+//   document.querySelector('#ranCount').innerText = i;
+//   if(i < 10){
+
+//     document.querySelector('#ranCount').innerText = "0" + i;
+//   }else{
+//     document.querySelector('#ranCount').innerText = i;
+//   }
+//  },1000);
+// }
+
+
+
+
